29f89626657867585071bdeb13f5727292c7208f
HP <HP@LAPTOP-GK4TO82F>
2022-10-05 20:11:10 +0700

    Add project files.

---
+++ Activity_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using sytem;
+
+namespace Bublbe_Sort
+{
+    class Program
+    {
+        //Deklarasi array int dengan ukuran 20
+        private int n;
+
+        // Fungsi / method untuk menerima masukan
+        public void read()
+        {
+            //menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write()
+            }
+        }
+    }
+}
